ac2733efacfd0c134696a98e05d10fa04f58b66b
tadeu1808 <tadeu1808@gmail.com>
2025-06-14 11:54:09 -0300

    sistema

---
+++ sistema.py
@@ -0,0 +1,12 @@
+def menu():
+    print("===============")
+    print("Sistema de cafe, cha e lasanha")
+    print("===============")
+    print("1. Criar um café")
+    print("2. Listar cafés" \
+    print("3. qual o maior preço do café")
+    print("3. Sair")
+    print("===============")
+    print("Escolha uma opção:")
+
+
